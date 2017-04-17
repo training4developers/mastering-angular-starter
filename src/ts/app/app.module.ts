88159e7eee632d91ccc9026816978e814ce3b547
Eric W. Greene <eric@ericwgreene.com>
2017-04-17 05:58:26 -0700

    tweaked config for vscode and made minor code tweaks

--- src/ts/app/app.module.ts
+++ src/ts/app/app.module.ts
@@ -8,6 +8,6 @@ import "../../scss/styles.scss";
 @NgModule({
     imports: [ BrowserModule ],
     declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
+    bootstrap: [ AppComponent ],
 })
 export class AppModule { }
